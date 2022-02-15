ef92a2abb951427322399fb5cf6aecb03225d3d8
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-14 23:26:07 -0300

    Added player instantiation and movement

---
+++ Assets/Scripts/GameManagement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+
+    private void Start()
+    {
+        if (playerPrefab != null)
+        {
+            Vector3 pos = new Vector3(0, 2, 0);
+            PhotonNetwork.Instantiate(playerPrefab.name, pos, Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Pun.Demo.PunBasics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviourPun
@@ -13,5 +14,44 @@ public class PlayerController : MonoBehaviourPun
     Rigidbody playerRb;
     Animator playerAnim;
 
-   
+    private void Awake()
+    {
+        speedMov = 7;
+        speedTurn = 3;
+    }
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        if (photonView.IsMine)
+            GetComponent<CameraWork>().OnStartFollowing();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+    }
+
+    private void Update()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        Vector3 newSpeed = transform.forward * inputV * speedMov;
+        newSpeed.y = playerRb.velocity.y;
+        playerRb.velocity = newSpeed;
+
+        playerRb.angularVelocity = new Vector3(0, inputH * speedTurn, 0);
+    }
 }
