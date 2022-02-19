20990af783c248c06e7cb44fe5a7cc4bae6475d2
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-19 20:11:01 -0300

    Update ProjectileScript.cs

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class ProjectileScript : MonoBehaviour
+public class ProjectileScript : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
