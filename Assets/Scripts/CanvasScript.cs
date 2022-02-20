2f8edbfa2ffb8c5b4ddccfcf0de588e3bb33525b
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-20 20:29:24 -0300

    Adding ammo count text

---
+++ Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour
+{
+    public Text ammoText;
+
+    private void Update()
+    {
+        FireScript[] player = GameObject.FindObjectsOfType<FireScript>();
+        string playerAmmo = "Ammo: ";
+        foreach (FireScript fire in player)
+            playerAmmo += fire.ammo + "; ";
+        ammoText.text = playerAmmo;
+        
+        //FireScript[] players = GameObject.FindObjectsOfType<FireScript>();
+        //string TxtAmmo = "Ammo: ";
+        //foreach (FireScript fire in players)
+        //    TxtAmmo += "Ammo: " + fire.ammo;
+        //ammoText.text = TxtAmmo;
+    }
+}
